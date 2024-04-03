b569bcfda0b4dccf35b2da51eb878891d3c0611f
Daniel Rodriguez <d.rodriguezriv0@gmail.com>
2024-04-02 22:01:51 -0600

    Realizando el commit 2 de la Tarea1, agregando estructura del para la seleccion de dificultad
    Signed-off-by: Daniel Rodriguez <d.rodriguezriv0@gmail.com>

--- Tarea1/src/main.cpp
+++ Tarea1/src/main.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main() {
     int opcion;
+    int intentos;
 
     do {
-        cout << "Bienvenido al juego del Ahorcado" << endl;
+        cout << "\nBienvenido al juego del Ahorcado\n" << endl;
         cout << "1. Elegir la dificultad del juego" << endl;
         cout << "2. Iniciar el juego" << endl;
         cout << "3. Agregar palabras al diccionario" << endl;
         cout << "4. Ver diccionario de palabras" << endl;
-        cout << "5. Salir del programa" << endl;
+        cout << "5. Salir del programa\n" << endl;
         cout << "Ingrese una opcion: ";
         cin >> opcion;
 
         switch(opcion) {
             case 1:
-                cout << "Probando opcion 1" << endl;
+                cout << "\nSeleccione la dificultad:\n" << endl;
+                cout << "1. Facil (7 intentos)" << endl;
+                cout << "2. Intermedio (5 intentos)" << endl;
+                cout << "3. Dificil (3 intentos)\n" << endl;
+                cout << "Ingrese la opcion de dificultad: ";
+                cin >> intentos;
+
+                switch(intentos) {
+                    case 1:
+                        cout << "Probando opcion 1 de la dificultad" << endl;
+                        break;
+                    case 2:
+                        cout << "Probando opcion 2 de la dificultad" << endl;
+                        break;
+                    case 3:
+                        cout << "Probando opcion 3 de la dificultad" << endl;
+                        break;
+                    default:
+                        cout << "\nOpcion de dificultad no valida." << endl;
+                        break;
+                }
                 break;
             case 2:
                 cout << "Probando opcion 2" << endl;
@@ -29,11 +51,11 @@ int main() {
                 cout << "Probando opcion 4" << endl;
                 break;
             case 5:
-                cout << "Muchas gracias por jugar"<< endl;
-                cout << "Saliendo del programa..." << endl;
+                cout << "\nMuchas gracias por jugar"<< endl;
+                cout << "Saliendo del programa...\n" << endl;
                 break;
             default:
-                cout << "Opcion no valida, por favor intente de nuevo." << endl;
+                cout << "\nOpcion no valida, por favor intente de nuevo." << endl;
         }
     } while(opcion != 5);
 
